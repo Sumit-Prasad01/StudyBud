58ad7b2d01395b7965d2af1eb91d8b627003549f
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-08-07 17:24:51 +0530

    Add login functionality and update templates for improved user experience

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Room
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username = username)
+        except :
+            messages.error(request, "User dose not exists!")
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
-    rooms = Room.objects.all()
-    context =  {'rooms' : rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) | 
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+        )
+
+    topics = Topic.objects.all()
+
+    rooms_count = rooms.count()
+
+    context =  {'rooms' : rooms, 'topics' : topics, 'rooms_count' : rooms_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
