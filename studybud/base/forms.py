ac0d4762f547e4be7756c08ad9b4468ed6d35918
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-08-15 16:23:25 +0530

    Add HTML templates for settings, signup, and topics pages; include CSS styles for layout and components

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -5,9 +5,11 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host', 'participants']
 
 
 class MessageForm(ModelForm):
     class Meta:
         model = Message
         fields = '__all__'
+        exclude = ['user', 'room']
